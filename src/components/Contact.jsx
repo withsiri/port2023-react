fb8ff080087d4a5cea3dbaea9b60cd9d937c8c35
m2 <withsiri91@gmail.com>
2023-08-28 01:26:57 +0900

    😏 연락처영역 완료

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,51 @@
 import React from "react";
 
-const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+const contactText = [
+    {
+        link: "https://open.kakao.com",
+        title: "KAKAO : WITHSIRI91",
+    },
+    {
+        link: "mailto:withsiri91@gmail.com",
+        title: "MAIL : WITHSIRI91@gamil.com"
+    },
+];
 
-export default Contact
+const Contact = () => {
+    return (
+    <section id="contact">
+        <div className="contact_inner">
+            <h2 className="contact_title">contact</h2>
+            <div className="contact_lines top" aria-hidden="true">
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+            </div>
+            <div className="contact_text">
+                <div className="text">
+                    {contactText.map((contact, key) => (
+                        <div key={key}>
+                            <a href={contact.link} rel="noopener noreferrer" target="_blank">{contact.title}</a>
+                        </div>
+                    ))}
+                    </div>
+                </div>
+                <div className="contact_lines" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+            </div>
+        </section>
+  );
+};
+
+export default Contact;

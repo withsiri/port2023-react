7248af65ea65737142d4b8e4c94e1536b4a7373d
m2 <withsiri91@gmail.com>
2023-08-27 14:20:29 +0900

    인트로 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,35 +31,35 @@ const Header = () => {
 	}
 	return (
 		<header id="header" role="banner">
-		<div className="header_inner">
-			<div className="header_logo">
-				<h1>
-					<a href="/">
-					Portfolio<em>React</em>
-					</a>
-				</h1>
+			<div className="header_inner">
+				<div className="header_logo">
+					<h1>
+						<a href="/">
+						Portfolio<em>React</em>
+						</a>
+					</h1>
+				</div>
+				<nav className={`header_nav ${show ? "show" : ""}`} role="navigation" aria-label="메인메뉴" >
+					<ul>
+						{headerNav.map((nav, key) => (
+							<li key={key}>
+								<a href={nav.url}>{nav.title}</a>
+							</li>
+						))}
+					</ul>
+				</nav>
+				<div
+					className="header_nav_mobile"
+					id="headerToggle"
+					aria-controls="primary-menu"
+					aria-expanded={show ? "true" : "false"}
+					role="button"
+					tabIndex="0"
+					onClick={toggleMenu}
+				>
+					<span></span>
+				</div>
 			</div>
-			<nav className={`header_nav ${show ? "show" : ""}`} role="navigation" aria-label="메인메뉴" >
-				<ul>
-					{headerNav.map((nav, key) => (
-						<li key={key}>
-							<a href={nav.url}>{nav.title}</a>
-						</li>
-					))}
-				</ul>
-			</nav>
-			<div
-				className="header_nav_mobile"
-				id="headerToggle"
-				aria-controls="primary-menu"
-				aria-expanded={show ? "true" : "false"}
-				role="button"
-				tabIndex="0"
-				onClick={toggleMenu}
-			>
-				<span></span>
-			</div>
-		</div>
 		</header>
 	);
 };
